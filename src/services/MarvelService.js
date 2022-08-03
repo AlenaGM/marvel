f7dad2d46a1046f50fa3e42dcd76baf254392bc4
AlenaGM <alena.guillaume4@gmail.com>
2022-08-03 13:05:26 +0200

    improoved api queries

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -14,14 +14,12 @@ class MarvelService {
 
   getAllCharacters = () => {
     return this.getResource(
-      "https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=4940e9cbf92c4bbc34109f7eaa55c672"
+      `${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`
     );
   };
 
   getCharacter = (id) => {
-    return this.getResource(
-      `https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=4940e9cbf92c4bbc34109f7eaa55c672`
-    );
+    return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
   };
 }
 
