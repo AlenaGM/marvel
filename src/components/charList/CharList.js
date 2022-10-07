f17b7029a516925c7ee6de7af2c1d83b6481db50
AlenaGM <alena.guillaume4@gmail.com>
2022-10-07 16:20:42 +0200

    upd

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -12,6 +12,7 @@ class CharList extends Component {
     error: false,
     newItemLoading: false,
     offset: 210,
+    charEnded: false,
   };
 
   marvelService = new MarvelService();
@@ -34,19 +35,24 @@ class CharList extends Component {
     });
   };
 
-  onCharListLoaded = (offset, newCharList) => {
-    this.setState(({ charList }) => ({
+  onCharListLoaded = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+    this.setState(({ offset, charList }) => ({
       charList: [...charList, ...newCharList],
       loading: false,
       newItemLoading: false,
       offset: offset + 9,
+      charEnded: ended,
     }));
   };
 
   onError = () => {
     this.setState({
-      loading: false,
       error: true,
+      loading: false,
     });
   };
 
@@ -74,7 +80,8 @@ class CharList extends Component {
   }
 
   render() {
-    const { charList, loading, error, offset, newItemLoading } = this.state;
+    const { charList, loading, error, offset, newItemLoading, charEnded } =
+      this.state;
 
     const items = this.renderItems(charList);
 
@@ -90,6 +97,7 @@ class CharList extends Component {
         <button
           className="button button__main button__long"
           disabled={newItemLoading}
+          style={{ display: charEnded ? "none" : "block" }}
           onClick={() => this.onRequest(offset)}
         >
           <div className="inner">load more</div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -32,7 +32,7 @@ class MarvelService {
       id: char.id,
       name: char.name,
       description: char.description
-        ? `${char.description.slice(0, 200)}...`
+        ? `${char.description.slice(0, 210)}...`
         : "There is no description for this character",
       thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
       homepage: char.urls[0].url,
