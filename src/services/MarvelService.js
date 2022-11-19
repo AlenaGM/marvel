b103dab38031c30a9732444ddeab86f0f8327317
AlenaGM <alena.guillaume4@gmail.com>
2022-11-19 14:34:20 +0100

    upd

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -36,10 +36,11 @@ const useMarvelService = () => {
       id: char.id,
       name: char.name,
       description: char.description
-        ? `${char.description.slice(0, 291)}...`
+        ? `${char.description.slice(0, 294)}...`
         : "There is no description for this character",
       thumbnail: char.thumbnail.path + "." + char.thumbnail.extension,
       homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
       comics: char.comics.items,
     };
   };
